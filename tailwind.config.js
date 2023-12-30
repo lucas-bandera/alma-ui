9701c4f6f0d16cc0b8f01c79c476b9466fae5545
Lucas Bandeira <lucasbandeira.mj@outlook.com>
2023-12-30 18:52:02 -0300

    feat: finished configuration and added Button component.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@ const { fontFamily } = require("tailwindcss/defaultTheme")
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
-  content: ["app/**/*.{ts,tsx}", "components/**/*.{ts,tsx}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  safelist: [
+    'dark'
+  ],
   theme: {
     container: {
       center: true,
